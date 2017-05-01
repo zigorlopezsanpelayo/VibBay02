d54e1417529d8d085212a4fabb7ff50b93f5ec20
zigorlopezsanpelayo <zigorlopezsanpelayo@iMac-de-Zigor.local>
2017-05-01 16:47:48 +0200

    Pujas

--- VibBayZA/app/src/main/java/com/example/zigorlopezsanpelayo/vibbayza/FragmentoPrincipal.java
+++ VibBayZA/app/src/main/java/com/example/zigorlopezsanpelayo/vibbayza/FragmentoPrincipal.java
@@ -1,12 +1,41 @@
 package com.example.zigorlopezsanpelayo.vibbayza;
 
+import android.content.DialogInterface;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
+import android.text.InputType;
+import android.util.Base64;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class FragmentoPrincipal extends Fragment {
+
+    protected boolean encontrado;
+    protected TextView articuloEncontrado;
+    protected LinearLayout arts;
+    protected ImageView imagenArticulo;
+
+    DatabaseReference refArticulos =
+            FirebaseDatabase.getInstance().getReference()
+                    .child("articulos");
 
     public FragmentoPrincipal() {
         // Required empty public constructor
@@ -19,5 +48,7 @@ public class FragmentoPrincipal extends Fragment {
         return inflater.inflate(R.layout.fragmento_principal, container, false);
 
     }
+
+
 
 }
